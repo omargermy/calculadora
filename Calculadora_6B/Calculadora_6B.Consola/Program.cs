413ff9a87bb2c498dfc6e0143f1b15eb2f3a6742
omargermy <germy_btmo@hotmail.com>
2017-08-22 18:24:16 -0500

    calculadora

---
+++ Calculadora_6B/Calculadora_6B.Consola/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculadora_6B.Libreria;
+
+namespace Calculadora_6B.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos MisMetodos = new Metodos();
+            //Console.WriteLine(MisMetodos.Saludar());
+            //Console.ReadKey();
+            Console.WriteLine("Introduzca El Primer Valor");
+            float Valor1;
+            Valor1 = float.Parse(Console.ReadLine()); //Lectura de datos tipo float
+            //Valor1 = MisMetodos.Sumar(Valor1,0);
+            //MisMetodos.Sumar(Valor1, 0);
+            //Valor1 = Console.ReadLine();
+            //Console.WriteLine("El Valor 1 Es: " + Valor1);
+            //Console.ReadKey();
+            Console.WriteLine("Introduzca El Segundo Valor");
+            float Valor2;
+            Valor2 = float.Parse(Console.ReadLine()); //Lectura de datos tipo float
+            //Valor1 = MisMetodos.Sumar(0, Valor2);
+            //MisMetodos.Sumar(0, Valor2);
+            //Valor2 = Console.ReadLine();
+            //Console.WriteLine("El Valor 2 Es: " + Valor2);
+            //MisMetodos.Sumar(Valor1, Valor2);
+            //Console.WriteLine("Resultado" + MisMetodos.Sumar());
+            Console.WriteLine("\nResultados: ");
+            Console.WriteLine("\nSuma: " + MisMetodos.Sumar(Valor1, Valor2));
+            Console.WriteLine("Resta: " + MisMetodos.Restar(Valor1, Valor2));
+            Console.WriteLine("Multiplicacion: " + MisMetodos.Multiplicar(Valor1, Valor2));
+            Console.WriteLine("Division: " + MisMetodos.Dividir(Valor1, Valor2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Calculadora_6B/Calculadora_6B.Libreria/Metodos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_6B.Libreria
+{
+    public class Metodos
+    {
+        /// <summary>
+        /// Metodo Publico Que Envia Saludo
+        /// </summary>
+        /// <returns>Una Cadena</returns>
+        public string Saludar()
+        {
+            return "Hola Mundo";
+        }
+        /// <summary>
+        /// Funcion Que Realiza La Suma De Dos Números
+        /// </summary>
+        /// <param name="Valor1">Flotante</param>
+        /// <param name="Valor2">Flotante</param>
+        /// <returns>Resultado Flotante</returns>
+        public float Sumar(float Valor1, float Valor2)
+        {
+            var Resultado = Valor1 + Valor2;
+            return Resultado;
+        }
+        /// <summary>
+        /// Funcion Que Realiza La Resta De Dos Números
+        /// </summary>
+        /// <param name="Valor1">Flotante</param>
+        /// <param name="Valor2">Flotante</param>
+        /// <returns>Resultado Flotante</returns>
+        public float Restar(float Valor1, float Valor2)
+        {
+            var Resultado = Valor1 - Valor2;
+            return Resultado;
+        }
+        /// <summary>
+        /// Funcion Que Realiza La Multiplicacion De Dos Números
+        /// </summary>
+        /// <param name="Valor1">Flotante</param>
+        /// <param name="Valor2">Flotante</param>
+        /// <returns>Resultado Flotante</returns>
+        public float Multiplicar(float Valor1, float Valor2)
+        {
+            var Resultado = Valor1 * Valor2;
+            return Resultado;
+        }
+        /// <summary>
+        /// Funcion Que Realiza La Division De Dos Números
+        /// </summary>
+        /// <param name="Valor1">Flotante</param>
+        /// <param name="Valor2">Flotante</param>
+        /// <returns>Resultado Flotante</returns>
+        public float Dividir(float Valor1, float Valor2)
+        {
+            var Resultado = Valor1 / Valor2;
+            return Resultado;
+        }
+    }
+}
